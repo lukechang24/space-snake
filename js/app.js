5db1f4e75e11e4c21b63899552db717a6d4a1cd7
Luke Chang <lukechang244@gmail.com>
2019-10-10 14:31:44 -0700

    added readme

--- js/app.js
+++ js/app.js
@@ -28,11 +28,13 @@ let returnToNormal;
 let warnings = [];
 let asteroids = [];
 let asteroidInterval;
+let bossSpawned;
 
 let lasers = [];
 let laserInterval;
-let firstAmmoText = false;
 let ammo;
+let currentAmmo;
+let firstAmmoText = false;
 
 let difficulty;
 let isPlaying;
@@ -44,8 +46,11 @@ let scoreDiv = document.querySelector("#score p");
 let highScoreDiv = document.querySelector("#high-score p")
 let highScore = localStorage.getItem("highestScore") ? localStorage.getItem("highestScore") : 0;
 
+let laserSound = new Audio("audio/laser-sound.wav");
+let gameOverSound = new Audio("audio/gameover-sound.wav")
+
 document.addEventListener("keydown", (e) => {
-    if(isPlaying === true) {
+    if(isPlaying) {
         return;
     } else if(e.which === 32) {
         startGame(e.which);
@@ -59,7 +64,7 @@ function startGame(whichKey) {
         head.style.top = "0px";
         snake.forEach((snakeBlock,i) => {
             if(i !== 0) {
-                snakeBlock.parentElement.removeChild(snakeBlock);
+                snakeBlock.parentNode.removeChild(snakeBlock);
             }
         })
         snake = [head]
@@ -72,7 +77,9 @@ function startGame(whichKey) {
         difficulty = 1;
         hit = false;
         difficulty = 1;
+        bossSpawned = false;
         foodCount = 0;
+        whichPowerUp = null;
         poweredUp = null;
         highScoreDiv.style.visibility = "hidden";
         powerUpStorage.src = "";
@@ -88,6 +95,9 @@ function startGame(whichKey) {
 startGame();
 
 document.addEventListener("keydown", (e) => {
+    if(!isPlaying) {
+        return;
+    }
     if(e.which === 37 && currentDirection !== "right" && currentHeadPositionArr[1] !== previousHeadPositionArr[1]) {
         currentDirection = "left";
     }
@@ -100,10 +110,11 @@ document.addEventListener("keydown", (e) => {
     if(e.which === 40 && currentDirection !== "up" && currentHeadPositionArr[0] !== previousHeadPositionArr[0]) {
         currentDirection = "down";
     }
-    if(e.which === 65 && whichPowerUp) {
+    if(e.which === 65 && whichPowerUp && !poweredUp) {
         applyPowerUp();
     }
     if(e.which === 83 && ammo !== 0) {
+        laserSound.play();
         createLasers();
         ammo--;
         applySnakePattern();
@@ -169,23 +180,22 @@ function removeFood() {
     food = null;
 }
 
-function createAsteroids() {
+function createAsteroids(pos, dir) {
     let blink = 0;
-    let spawnTopOrBottom = parseInt(`${difficulty === 1 ? 0 : Math.floor(Math.random()*2)}`);
-    let randomPosition = Math.round((Math.random()*(map.offsetWidth-20))/20)*20 + "px";
     let indicateAsteroid = document.createElement("img");
-    indicateAsteroid.src = "images/warning1.png";
+    indicateAsteroid.src = "images/warning2.png";
     indicateAsteroid.className = "warning";
     map.appendChild(indicateAsteroid);
     indicateAsteroid.style.height = "20px";
     indicateAsteroid.style.width = "20px";
     indicateAsteroid.style.position = "absolute";
-    indicateAsteroid.style.left = randomPosition;
-    indicateAsteroid.style.top = `${spawnTopOrBottom === 0 ? 0 : map.offsetHeight-indicateAsteroid.offsetHeight}px`;
+    // indicateAsteroid.style.left = pos;
+    indicateAsteroid.style.left = `${dir === 2 ? 0 : dir === 3 ? map.offsetWidth-20 : pos}px`
+    indicateAsteroid.style.top = `${dir === 0 ? 0 : dir === 1 ? map.offsetHeight-indicateAsteroid.offsetHeight : pos}px`;
     warnings.push(indicateAsteroid)
     let newAsteroid = document.createElement("div");
-    newAsteroid.className = `${spawnTopOrBottom === 0 ? "asteroid-top" : "asteroid-bottom"}`;
-    newAsteroid.style.backgroundImage = `${spawnTopOrBottom === 0 ? "linear-gradient(red, grey, grey)" : "linear-gradient(grey, grey, red)"}`
+    newAsteroid.className = `${dir === 0 ? "asteroid-top" : dir === 1 ?  "asteroid-bottom" : dir === 2 ? "asteroid-left" : "asteroid-right"}`;
+    newAsteroid.style.backgroundImage = `${dir === 0 ? "linear-gradient(red, grey, grey)" : dir === 1 ? "linear-gradient(grey, grey, red)" : dir === 2 ? "linear-gradient(to right, red, grey, grey)" : "linear-gradient(to right, grey, grey, red)"}`;
     newAsteroid.style.border = "1.5px solid red";
     newAsteroid.style.borderRadius = "5px";
     newAsteroid.style.position = "absolute";
@@ -200,8 +210,8 @@ function createAsteroids() {
         if(blink === 5) {
             indicateAsteroid.parentNode.removeChild(indicateAsteroid);
             map.appendChild(newAsteroid);
-            newAsteroid.style.left = randomPosition;
-            newAsteroid.style.top = `${newAsteroid.className === "asteroid-top" ? 0 : (map.offsetHeight-newAsteroid.offsetHeight)}px`;
+            newAsteroid.style.left = `${newAsteroid.className === "asteroid-left" ? 0 : newAsteroid.className === "asteroid-right" ? map.offsetWidth-newAsteroid.offsetWidth : pos}px`;
+            newAsteroid.style.top = `${newAsteroid.className === "asteroid-top" ? 0 : newAsteroid.className === "asteroid-bottom" ? (map.offsetHeight-newAsteroid.offsetHeight) : pos}px`;
             asteroids.push(newAsteroid);
             clearInterval(warningSignal);
         }
@@ -217,14 +227,25 @@ function createAsteroids() {
 function moveAsteroids() {
     let asteroidsClone = [...asteroids];
     asteroidsClone.forEach(asteroid => {
-        asteroid.style.top = `${asteroid.className === "asteroid-top" ? (asteroid.offsetTop+20) : (asteroid.offsetTop-20)}px`;
+        if(asteroid.className === "asteroid-top") {
+            asteroid.style.top = (asteroid.offsetTop+20)+"px";
+        }
+        if(asteroid.className === "asteroid-bottom") {
+            asteroid.style.top = (asteroid.offsetTop-20)+"px";
+        }
+        if(asteroid.className === "asteroid-left") {
+            asteroid.style.left = (asteroid.offsetLeft+20)+"px";
+        }
+        if(asteroid.className === "asteroid-right") {
+            asteroid.style.left = (asteroid.offsetLeft-20)+"px";
+        }
         snake.forEach(snakeBody => {
-            if(asteroid.offsetTop === snakeBody.offsetTop && asteroid.offsetLeft == snakeBody.offsetLeft) {
+            if(asteroid.offsetTop === snakeBody.offsetTop && asteroid.offsetLeft === snakeBody.offsetLeft) {
                 endGame();
             }
         })
-        if((asteroid.offsetTop+asteroid.offsetHeight > (map.offsetTop-interfaceHeight)+map.offsetHeight && asteroid.className === "asteroid-top") || (asteroid.offsetTop < (map.offsetTop-interfaceHeight) && asteroid.className === "asteroid-bottom")) {
-            asteroid.parentElement.removeChild(asteroid);
+        if((asteroid.offsetTop+asteroid.offsetHeight > (map.offsetTop-interfaceHeight)+map.offsetHeight && asteroid.className === "asteroid-top") || (asteroid.offsetTop < (map.offsetTop-interfaceHeight) && asteroid.className === "asteroid-bottom") || (asteroid.offsetLeft+asteroid.offsetWidth > map.offsetLeft+map.offsetWidth && asteroid.className === "asteroid-left") || (asteroid.offsetLeft < map.offsetLeft && asteroid.className === "asteroid-right")) {
+            asteroid.parentNode.removeChild(asteroid);
             asteroids.shift();
             warnings.shift();
         }
@@ -233,17 +254,36 @@ function moveAsteroids() {
 
 function callDownAsteroids() {
     asteroidInterval = setInterval(() => {
-        difficulty = score > 3000 ? 3 : score > 1500 ? 2 : 1;
-        // let chance = Math.random()*10+1;
-        if(warnings.length < difficulty) {
-        createAsteroids();
+        let chanceToSpawn = Math.random()*10;
+        difficulty = score > 2000 ? 3 : score > 1000 ? 2 : 1;
+        if(chanceToSpawn > 6 && warnings.length < difficulty) {
+            let whichDir = 3;
+            let randomPosition = `${whichDir === 0 || whichDir === 1 ? Math.round((Math.random()*(map.offsetWidth-20))/20)*20 : Math.round((Math.random()*(map.offsetHeight-20))/20)*20}`;
+            if(score > 200 && !bossSpawned) {
+                createBoss(whichDir);
+                return;
+            }
+            createAsteroids(randomPosition, 3);
         }
         moveAsteroids();
     },1000);
 }
 
+function createBoss(dir) {
+    if(dir === 0 || dir === 1) {
+        for(let i = 0; i < map.offsetWidth/20; i++) {
+            createAsteroids((i*20), dir);
+        }
+    } else {
+        for(let i = 0; i < map.offsetHeight/20; i++) {
+            createAsteroids((i*20), dir);
+        }
+    }
+    bossSpawned = true;
+}
+
 function removeAsteroids() {
-    while (asteroids.length > 0) {
+    while(asteroids.length > 0) {
         asteroids[0].parentNode.removeChild(asteroids[0]);
         asteroids.shift();
     }
@@ -283,10 +323,10 @@ function moveLasers() {
                 laser.style.top = (laser.offsetTop+20)+"px";
             }
             asteroids.forEach(asteroid => {
-                if(laser.offsetTop === asteroid.offsetTop && laser.offsetLeft === asteroid.offsetLeft) {
-                    laser.parentElement.removeChild(laser);
+                if(((laser.offsetTop === asteroid.offsetTop || laser.offsetTop === asteroid.offsetTop-20 && laser.className === "laser-top") && laser.offsetLeft === asteroid.offsetLeft) || ((laser.offsetTop === asteroid.offsetTop || laser.offsetTop === asteroid.offsetTop+20 && laser.className === "laser-down") && laser.offsetLeft === asteroid.offsetLeft) || ((laser.offsetLeft === asteroid.offsetLeft || laser.offsetLeft === asteroid.offsetLeft-20 && laser.className === "laser-left") && laser.offsetTop === asteroid.offsetTop) || ((laser.offsetLeft === asteroid.offsetLeft || laser.offsetLeft === asteroid.offsetLeft+20 && laser.className === "laser-right")&& laser.offsetTop === asteroid.offsetTop)) {
+                    laser.parentNode.removeChild(laser);
                     lasers.splice(lasers.indexOf(laser), 1);
-                    asteroid.parentElement.removeChild(asteroid);
+                    asteroid.parentNode.removeChild(asteroid);
                     asteroids.splice(asteroids.indexOf(asteroid), 1)
                     warnings.shift();
                     score += 50;
@@ -294,7 +334,7 @@ function moveLasers() {
                 }
             })
             if((laser.offsetLeft < map.offsetLeft && laser.className === "laser-left") || (laser.offsetLeft+laser.offsetWidth > (map.offsetLeft+map.offsetWidth) && laser.className === "laser-right") || (laser.offsetTop+laser.offsetHeight > (map.offsetTop-interfaceHeight)+map.offsetHeight && laser.className === "laser-down") || (laser.offsetTop < (map.offsetTop-interfaceHeight) && laser.className === "laser-up")) {
-                laser.parentElement.removeChild(laser);
+                laser.parentNode.removeChild(laser);
                 lasers.splice(lasers.indexOf(laser), 1);
             }
         })
@@ -313,6 +353,13 @@ function displayAmmoText() {
         ammoText.parentNode.removeChild(ammoText);
     },1500);
     firstAmmoText = true;
+}
+
+function removeLasers() {
+    while(lasers.length > 0) {
+        lasers[0].parentNode.removeChild(lasers[0]);
+        lasers.shift();
+    }
 }
 
 function createPowerUp() {
@@ -345,7 +392,7 @@ function spawnPowerUp() {
 function storePowerUp() {
     let randomNum = Math.floor(Math.random()*3);
     if(randomNum === 0) {
-        whichPowerUp = "speed";
+        whichPowerUp = "rapid-fire";
         powerUpStorage.src = "images/speed-up.png";
     } else if(randomNum === 1){
         whichPowerUp = "slow";
@@ -360,12 +407,14 @@ function storePowerUp() {
 }
 
 function applyPowerUp() {
-    if(whichPowerUp === "speed") {
-        speedUpSnake();
+    if(whichPowerUp === "rapid-fire") {
+        rapidFireSnake();
     } else if(whichPowerUp === "slow"){
         slowDownSnake();
     } else if(whichPowerUp === "freeze"){
-        freezeTime()
+        freezeTime();
+    } else {
+        return;
     }
     clearInterval(move);
     startMoving();
@@ -377,6 +426,9 @@ function applyPowerUp() {
         }
         speed = 75;
         scoreMultiplier = 1;
+        if(whichPowerUp === "rapid-fire") {
+            ammo = currentAmmo;
+        }
         poweredUp = false;
         whichPowerUp = null;
         powerUp = null;
@@ -389,7 +441,7 @@ function applyPowerUp() {
 
 function displayPowerUpText() {
     let powerUpText = document.createElement("div");
-    powerUpText.innerText = `${whichPowerUp === "speed" ? "SPEED UP (2x SCORE)" : whichPowerUp === "slow" ? "SLOW DOWN" : "FREEZE TIME"}${!firstPowerUpText ? " (PRESS 'A' TO USE)" : ""}`;
+    powerUpText.innerText = `${whichPowerUp === "rapid-fire" ? "RAPID FIRE (2x SCORE)" : whichPowerUp === "slow" ? "DECREASE SPEED" : "FREEZE TIME"}${!firstPowerUpText ? " (PRESS 'A' TO USE)" : ""}`;
     powerUpText.style.position = "absolute";
     powerUpText.style.color = "white";
     powerUpText.style.left = head.offsetLeft + "px";
@@ -397,16 +449,18 @@ function displayPowerUpText() {
     map.appendChild(powerUpText);
     const removePowerUpText = setTimeout(() => {
         powerUpText.parentNode.removeChild(powerUpText);
-    },1500);
+    },3000);
     firstPowerUpText = true;
 }
 
-function speedUpSnake() {
-    speed = 50;
+function rapidFireSnake() {
     scoreMultiplier = 2;
     snake.forEach((block, i) => {
-        block.style.backgroundColor = `${i % 6 === 0 ? "purple" : i % 5 === 0 ? "blue" : i % 4 === 0 ? "green" : i % 3 === 0 ? "yellow" : i % 2 === 0 ? "orange" : "red"}`
+        // block.style.backgroundColor = `${i % 6 === 0 ? "purple" : i % 5 === 0 ? "blue" : i % 4 === 0 ? "green" : i % 3 === 0 ? "yellow" : i % 2 === 0 ? "orange" : "red"}`;
+        block.style.backgroundColor = "gold";
     })
+    currentAmmo = ammo;
+    ammo = 1000;
 }
 
 function slowDownSnake() {
@@ -431,16 +485,20 @@ function applySnakePattern() {
         if(i !== 0) {
             if(i <= ammo) {
                 block.style.border = "1.5px solid yellow";
+                block.style.backgroundColor = "yellow";
             } else {
                 block.style.border = "1.5px solid black";
+                block.style.backgroundColor = "red";
             }
         }
-        if(whichPowerUp === "speed" && poweredUp) {
-            block.style.backgroundColor = `${i % 6 === 0 ? "purple" : i % 5 === 0 ? "blue" : i % 4 === 0 ? "green" : i % 3 === 0 ? "yellow" : i % 2 === 0 ? "orange" : "red"}`;
+        if(whichPowerUp === "rapid-fire" && poweredUp) {
+            // block.style.backgroundColor = `${i % 6 === 0 ? "purple" : i % 5 === 0 ? "blue" : i % 4 === 0 ? "green" : i % 3 === 0 ? "yellow" : i % 2 === 0 ? "orange" : "red"}`;
+            block.style.backgroundColor = "gold";
         } else if(whichPowerUp === "slow" && poweredUp) {
             block.style.backgroundColor = "purple";
         } else {
-            block.style.backgroundColor = `${i % 3 === 0 ? "#00FFFF" : "#00B2EE"}`
+            // block.style.backgroundColor = `${block.style.backgroundColor === "yellow" ? "yellow" : i % 3 === 0 ? "#00FFFF" : "#00B2EE"}`
+            block.style.backgroundColor = `${block.style.backgroundColor === "yellow" ? "yellow" : "#00B2EE"}`
         }
     })
 }
@@ -453,14 +511,14 @@ function growSnake() {
     newBody.style.position = "absolute";
     newBody.style.height = "17px";
     newBody.style.width = "17px";
+    applySnakePattern();
     map.appendChild(newBody);
     newBody.style.left = currentPosition[0] + "px";
     newBody.style.top = currentPosition[1] + "px";
     snake.push(newBody);
-    if(snake.length % 5 === 0) {
+    if(snake.length % 10 === 0) {
         spawnPowerUp();
     }
-    applySnakePattern();
 }
 
 function detectCollision() {
@@ -540,6 +598,9 @@ function endGame() {
     if(powerUp) {
         removePowerUp();
     }
+    if(lasers.length > 0) {
+        removeLasers();
+    }
     poweredUp = false;
     whichPowerUp = null;
     warnings = [];
@@ -560,4 +621,5 @@ function endGame() {
     highScoreDiv.style.visibility = "visible";
     isPlaying = false;
     renderScore();
+    gameOverSound.play();
 }
